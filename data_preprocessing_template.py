55b0daedf97121857d187a7b11b1af7be14b5d1c
jayapavandeshpande <jayapavanpd@gmail.com>
2021-01-17 12:10:31 +0530

    Update data_preprocessing_template.py

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -4,11 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # Importing the dataset
 # ---------------------
 dataset = pd.read_csv('../data_files/Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
 
 # Taking care of missing data
 # ---------------------------
@@ -16,6 +18,7 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
 
 # Encoding categorical data
 # -------------------------
@@ -27,10 +30,12 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 
+
 # Splitting the dataset into the Training set and Test set
 # --------------------------------------------------------
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
 
 # Feature scaling
 # ---------------
